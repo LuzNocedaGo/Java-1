7c7709383ca99510881a787438ef014f9b0933a1
Luz_NocedaGo <114294673+LuzNocedaGo@users.noreply.github.com>
2023-01-19 09:49:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let r = prompt('Введите радиус')
+let result = Math.PI * Math.pow(r, 2)
+console.log(result)
+
+
+
+let side = prompt('Введите длину стороны')
+let ress = side * 4
+console.log(ress)
